50a8b7749770ddbada3cbcd688107d00c8072256
manigandan-d <manigandan220916@gmail.com>
2024-04-14 12:40:47 +0530

    code

--- LED/main.c
+++ LED/main.c
@@ -1,17 +1,17 @@
 #include <reg51.h>
 
-void delay_ms(int time) {
+void delay(int n) {
 	int i, j;
 
-	for(i=0; i<time; i++)    
+	for(i=0; i<n; i++)    
 	for(j=0; j<1275; j++);
 }
 
 void main() {
 	while(1) {
 		P2 = 0x00;
-		delay_ms(100);
+		delay(100);
 		P2 = 0xff;
-		delay_ms(100);
+		delay(100);
 	}
 }
